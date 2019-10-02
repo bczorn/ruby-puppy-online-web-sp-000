ac2247c4b3e85ebf2aa4d3a2590b77748f4e6587
Becky Zorn <bczorn@gmail.com>
2019-10-02 05:50:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+class Dog
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save(name)
+  end
+  
+  def name
+    @name
+  end
+  
+  def save(name)
+    @@all << name
+  end
+  
+  def self.all
+    @@all.inspect
+  
+  end
+  def self.clear_all
+    @@all = []
+  end
+  def self.print_all
+    @@all.each do |x|
+      puts x
+    end
+  end
+end
